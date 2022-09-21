871223300cbefbfd164c45290db027b35a0f8240
rwong2888 <rwong@forbes.com>
2022-09-21 14:10:35 -0400

    username

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 )
 
 func main() {
-	myFigure := figure.NewColorFigure("<USERNAME> is Awesome!!!", "larry3d", "yellow", true)
+	myFigure := figure.NewColorFigure("<rwong2888> is Awesome!!!", "larry3d", "yellow", true)
 
 	if secret := os.Getenv("SECRET"); secret != "" {
 		myFigure = figure.NewColorFigure(fmt.Sprintf("Secret value is: %s", secret), "larry3d", "yellow", true)
